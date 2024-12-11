e1a93c1f0cf6ced9abfdb7c5c179dee68b3e09c5
mudasira42 <mudassirasultana42@gmail.com>
2024-12-11 16:45:01 +0530

    Add files via upload

---
+++ ollama_chatbot/src/main.py
@@ -0,0 +1,54 @@
+import os 
+import json
+
+import streamlit as st
+from groq import Groq
+
+
+st.set_page_config(
+    page_title="ChatBuddy",
+    page_icon="🤖",
+    layout="centered"
+)
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+config_data = json.load(open(f"{working_dir}/config.json"))
+
+GROQ_API_KEY = config_data["GROQ_API_KEY"]
+
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+client = Groq()
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+
+st.title("🤖 ChatBuddy ")
+
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+user_prompt = st.chat_input("Ask anything....")
+
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user" , "content": user_prompt})
+
+    messages = [
+        {"role": "system" , "content": "You are helpful assistant"},
+        *st.session_state.chat_history
+    ]
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=messages
+    )
+
+    assistant_response = response.choices[0].message.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
